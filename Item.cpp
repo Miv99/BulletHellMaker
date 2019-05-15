9a7e973f75df9ee7ec7319113079c56e881ab7b5
Miv99 <treecam42@gmail.com>
2019-05-15 02:33:32 -0700

    Add sound when item is collected

--- Item.cpp
+++ Item.cpp
@@ -1,4 +1,5 @@
 #include "Item.h"
+#include "LevelPack.h"
 
 std::shared_ptr<Item> ItemFactory::create(std::string formattedString) {
 	auto name = split(formattedString, DELIMITER)[0];
@@ -17,7 +18,7 @@ std::shared_ptr<Item> ItemFactory::create(std::string formattedString) {
 }
 
 std::string HealthPackItem::format() {
-	return "HealthPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius);
+	return "HealthPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius) + delim + "(" + onCollectSound.format() + ")";
 }
 
 void HealthPackItem::load(std::string formattedString) {
@@ -25,10 +26,16 @@ void HealthPackItem::load(std::string formattedString) {
 	animatable.load(items[1]);
 	hitboxRadius = std::stof(items[2]);
 	activationRadius = std::stof(items[3]);
+	onCollectSound.load(items[4]);
+}
+
+void HealthPackItem::onPlayerContact(entt::DefaultRegistry & registry, uint32_t player) {
+	Item::onPlayerContact(registry, player);
+	registry.get<HealthComponent>(player).heal(HEALTH_PER_HEALTH_PACK);
 }
 
 std::string PowerPackItem::format() {
-	return "PowerPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius);
+	return "PowerPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius) + delim + "(" + onCollectSound.format() + ")";
 }
 
 void PowerPackItem::load(std::string formattedString) {
@@ -36,10 +43,16 @@ void PowerPackItem::load(std::string formattedString) {
 	animatable.load(items[1]);
 	hitboxRadius = std::stof(items[2]);
 	activationRadius = std::stof(items[3]);
+	onCollectSound.load(items[4]);
+}
+
+void PowerPackItem::onPlayerContact(entt::DefaultRegistry & registry, uint32_t player) {
+	Item::onPlayerContact(registry, player);
+	registry.get<PlayerTag>().increasePower(registry, player, POWER_PER_POWER_PACK);
 }
 
 std::string PointsPackItem::format() {
-	return "PointsPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius);
+	return "PointsPackItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius) + delim + "(" + onCollectSound.format() + ")";
 }
 
 void PointsPackItem::load(std::string formattedString) {
@@ -47,10 +60,16 @@ void PointsPackItem::load(std::string formattedString) {
 	animatable.load(items[1]);
 	hitboxRadius = std::stof(items[2]);
 	activationRadius = std::stof(items[3]);
+	onCollectSound.load(items[4]);
+}
+
+void PointsPackItem::onPlayerContact(entt::DefaultRegistry & registry, uint32_t player) {
+	Item::onPlayerContact(registry, player);
+	registry.get<LevelManagerTag>().addPoints(POINTS_PER_POINTS_PACK);
 }
 
 std::string BombItem::format() {
-	return "BombItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius);
+	return "BombItem" + delim + "(" + animatable.format() + ")" + delim + tos(hitboxRadius) + delim + tos(activationRadius) + delim + "(" + onCollectSound.format() + ")";
 }
 
 void BombItem::load(std::string formattedString) {
@@ -58,4 +77,14 @@ void BombItem::load(std::string formattedString) {
 	animatable.load(items[1]);
 	hitboxRadius = std::stof(items[2]);
 	activationRadius = std::stof(items[3]);
+	onCollectSound.load(items[4]);
+}
+
+void BombItem::onPlayerContact(entt::DefaultRegistry & registry, uint32_t player) {
+	Item::onPlayerContact(registry, player);
+	registry.get<PlayerTag>().gainBombs(registry, 1);
+}
+
+void Item::onPlayerContact(entt::DefaultRegistry & registry, uint32_t player) {
+	registry.get<LevelManagerTag>().getLevelPack()->playSound(onCollectSound);
 }
--- Item.h
+++ Item.h
@@ -5,6 +5,9 @@
 #include "Constants.h"
 #include "TextMarshallable.h"
 #include "Player.h"
+#include "AudioPlayer.h"
+
+class LevelPack;
 
 /*
 An item is something that can be picked up by the player and does something on pickup.
@@ -15,16 +18,19 @@ class Item : public TextMarshallable {
 public:
 	inline Item() {}
 	inline Item(Animatable animatable, float hitboxRadius, float activationRadius = 75.0f) : animatable(animatable), hitboxRadius(hitboxRadius), activationRadius(activationRadius) {}
+	inline Item(Animatable animatable, float hitboxRadius, SoundSettings onCollectSound, float activationRadius = 75.0f) : animatable(animatable), hitboxRadius(hitboxRadius), onCollectSound(onCollectSound), activationRadius(activationRadius) {}
 
 	std::string format() = 0;
 	void load(std::string formattedString) = 0;
 
 	// Called when the player makes contact with an item's hitbox
-	virtual void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player) = 0;
+	virtual void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player);
 
 	inline Animatable getAnimatable() { return animatable; }
 	inline float getHitboxRadius() { return hitboxRadius; }
 	inline float getActivationRadius() { return activationRadius; }
+	// Returns a reference
+	inline SoundSettings& getOnCollectSound() { return onCollectSound; }
 
 	inline void setAnimatable(Animatable animatable) { this->animatable = animatable; }
 	inline void setHitboxRadius(float hitboxRadius) { this->hitboxRadius = hitboxRadius; }
@@ -36,6 +42,9 @@ protected:
 	float hitboxRadius;
 	// Minimum distance between an item and player before the item begins moving towards the player
 	float activationRadius;
+
+	// Sound to be played when item is collected by the player
+	SoundSettings onCollectSound;
 };
 
 /*
@@ -45,13 +54,12 @@ class HealthPackItem : public Item {
 public:
 	inline HealthPackItem() : Item() {}
 	inline HealthPackItem(Animatable animatable, float hitboxRadius, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, activationRadius) {}
+	inline HealthPackItem(Animatable animatable, float hitboxRadius, SoundSettings onCollectSound, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, onCollectSound, activationRadius) {}
 
 	std::string format() override;
 	void load(std::string formattedString) override;
 
-	inline void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player) {
-		registry.get<HealthComponent>(player).heal(HEALTH_PER_HEALTH_PACK);
-	}
+	void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player);
 };
 
 /*
@@ -61,13 +69,12 @@ class PowerPackItem : public Item {
 public:
 	inline PowerPackItem() : Item() {}
 	inline PowerPackItem(Animatable animatable, float hitboxRadius, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, activationRadius) {}
+	inline PowerPackItem(Animatable animatable, float hitboxRadius, SoundSettings onCollectSound, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, onCollectSound, activationRadius) {}
 
 	std::string format() override;
 	void load(std::string formattedString) override;
 
-	inline void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player) {
-		registry.get<PlayerTag>().increasePower(registry, player, POWER_PER_POWER_PACK);
-	}
+	void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player);
 };
 
 /*
@@ -77,13 +84,12 @@ class BombItem : public Item {
 public:
 	inline BombItem() : Item() {}
 	inline BombItem(Animatable animatable, float hitboxRadius, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, activationRadius) {}
+	inline BombItem(Animatable animatable, float hitboxRadius, SoundSettings onCollectSound, float activationRadius = 75.0f) : Item(animatable, hitboxRadius, onCollectSound, activationRadius) {}
 
 	std::string format() override;
 	void load(std::string formattedString) override;
 
-	inline void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player) {
-		registry.get<PlayerTag>().gainBombs(registry, 1);
-	}
+	void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player);
 };
 
 /*
@@ -93,13 +99,12 @@ class PointsPackItem : public Item {
 public:
 	inline PointsPackItem() : Item() {}
 	inline PointsPackItem(Animatable animatable, float hitboxRadius, float activationRadius = 150.0f) : Item(animatable, hitboxRadius, activationRadius) {}
+	inline PointsPackItem(Animatable animatable, float hitboxRadius, SoundSettings onCollectSound, float activationRadius = 150.0f) : Item(animatable, hitboxRadius, onCollectSound, activationRadius) {}
 
 	std::string format() override;
 	void load(std::string formattedString) override;
 
-	inline void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player) {
-		registry.get<LevelManagerTag>().addPoints(POINTS_PER_POINTS_PACK);
-	}
+	void onPlayerContact(entt::DefaultRegistry& registry, uint32_t player);
 };
 
 class ItemFactory {
