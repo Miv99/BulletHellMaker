6475818b90dc0d0a8e3fce20379255986ceaff4f
miv99 <treecam42@gmail.com>
2020-01-03 16:39:35 -0800

    Fix collectibles not moving

--- Components.cpp
+++ Components.cpp
@@ -623,9 +623,9 @@ void CollectibleComponent::activate(EntityCreationQueue& queue, entt::DefaultReg
 
 	auto speed = std::make_shared<PiecewiseTFV>();
 	// Speed starts off quickly at 450 and slows to 350 by t=2
-	speed->insertSegment(0, std::make_pair(2.0f, std::make_shared<DampenedEndTFV>(450.0f, 350.0f, 2.0f, 2)));
+	speed->insertSegment(std::make_pair(0, std::make_shared<DampenedEndTFV>(450.0f, 350.0f, 2.0f, 2)));
 	// Speed then maintains a constant 350 forever
-	speed->insertSegment(1, std::make_pair(std::numeric_limits<float>::max() - 3, std::make_shared<ConstantTFV>(350.0f)));
+	speed->insertSegment(std::make_pair(2.0f, std::make_shared<ConstantTFV>(350.0f)));
 	auto newPath = std::make_shared<HomingMP>(std::numeric_limits<float>::max(), speed, std::make_shared<ConstantTFV>(0.12f), self, registry.attachee<PlayerTag>(), registry);
 	registry.get<MovementPathComponent>(self).setPath(queue, registry, self, registry.get<PositionComponent>(self), newPath, 0);
 	// Make sure item can't despawn
