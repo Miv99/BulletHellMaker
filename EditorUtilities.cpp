4c70113e3ca1f2d48c12e4b71a19f2e9f72a719c
Miv99 <treecam42@gmail.com>
2020-01-28 20:38:21 -0800

    Add ClickableTimeline widget

--- EditorUtilities.cpp
+++ EditorUtilities.cpp
@@ -1718,3 +1718,123 @@ void TabsWithPanel::updateMoreTabsList() {
 
 	moreTabsButton->setVisible(tabsOrdering.size() > 0);
 }
+
+ClickableTimeline::ClickableTimeline() {
+	cameraController = tgui::RangeSlider::create();
+	cameraController->setPosition(6, 6);
+	cameraController->connect("RangeChanged", [&](float lower, float upper) {
+		if ((upper - lower) / maxTimeValue < 0.01) {
+			cameraController->setSelectionEnd(lower + maxTimeValue * 0.01);
+			cameraController->setSelectionStart(upper - maxTimeValue * 0.01);
+			return;
+		}
+
+		buttonScalarScalar = 1 / ((upper - lower)/maxTimeValue);
+		panel->setHorizontalScrollbarValue(lower * buttonScalar * buttonScalarScalar);
+		
+		updateButtonsPositionsAndSizes();
+	});
+	add(cameraController);
+
+	panel = tgui::ScrollablePanel::create();
+	panel->setHorizontalScrollbarPolicy(tgui::Scrollbar::Policy::Never);	
+	panel->setVerticalScrollbarPolicy(tgui::Scrollbar::Policy::Never);
+	add(panel);
+
+	connect("SizeChanged", [&](sf::Vector2f newSize) {
+		cameraController->setSize(newSize.x - 12, SLIDER_HEIGHT);
+		panel->setSize("100%", newSize.y - SLIDER_HEIGHT - 5);
+		panel->setPosition(0, tgui::bindBottom(cameraController) + 5);
+		buttonScalar = newSize.x / maxTimeValue;
+
+		updateButtonsPositionsAndSizes();
+	});
+}
+
+void ClickableTimeline::setElements(std::vector<std::pair<float, std::string>> elementStartTimesAndDuration, float maxTime) {
+	if (elementStartTimesAndDuration.size() == 0) {
+		elements.clear();
+		panel->removeAllWidgets();
+		return;
+	}
+	
+	std::vector<std::tuple<float, float, std::string>> converted;
+	for (int i = 1; i < elementStartTimesAndDuration.size(); i++) {
+		converted.push_back(std::make_tuple(elementStartTimesAndDuration[i - 1].first, elementStartTimesAndDuration[i].first - elementStartTimesAndDuration[i - 1].first, elementStartTimesAndDuration[i - 1].second));
+	}
+	converted.push_back(std::make_tuple(elementStartTimesAndDuration[elementStartTimesAndDuration.size() - 1].first, maxTime - elementStartTimesAndDuration[elementStartTimesAndDuration.size() - 1].first, elementStartTimesAndDuration[elementStartTimesAndDuration.size() - 1].second));
+	setElements(converted, maxTime);
+}
+
+void ClickableTimeline::setElements(std::vector<std::tuple<float, float, std::string>> elementStartTimesAndDuration) {
+	elements.clear();
+	panel->removeAllWidgets();
+
+	if (elementStartTimesAndDuration.size() == 0) return;
+
+	maxTimeValue = std::get<0>(elementStartTimesAndDuration[elementStartTimesAndDuration.size() - 1]) + std::get<1>(elementStartTimesAndDuration[elementStartTimesAndDuration.size() - 1]);
+	buttonScalar = panel->getSize().x/maxTimeValue;
+
+	cameraController->setMaximum(maxTimeValue);
+	cameraController->setStep(maxTimeValue / 200.0f);
+	cameraController->setSelectionStart(0);
+	cameraController->setSelectionEnd(maxTimeValue);
+
+	int i = 0;
+	for (std::tuple<float, float, std::string> tuple : elementStartTimesAndDuration) {
+		std::shared_ptr<tgui::Button> button = tgui::Button::create();
+		button->setText(std::get<2>(tuple));
+		button->connect("Pressed", [&, i]() {
+			onElementPressed.emit(this, i);
+		});
+		panel->add(button);
+		elements.push_back(std::make_tuple(std::get<0>(tuple), std::get<1>(tuple), button));
+		i++;
+	}
+
+	updateButtonsPositionsAndSizes();
+}
+
+void ClickableTimeline::setElements(std::vector<std::tuple<float, float, std::string>> elementStartTimesAndDuration, float maxTime) {
+	elements.clear();
+	panel->removeAllWidgets();
+
+	if (elementStartTimesAndDuration.size() == 0) return;
+
+	maxTimeValue = maxTime;
+	buttonScalar = panel->getSize().x / maxTimeValue;
+
+	cameraController->setMaximum(maxTimeValue);
+	cameraController->setStep(maxTimeValue / 200.0f);
+	cameraController->setSelectionStart(0);
+	cameraController->setSelectionEnd(maxTimeValue);
+
+	int i = 0;
+	for (std::tuple<float, float, std::string> tuple : elementStartTimesAndDuration) {
+		std::shared_ptr<tgui::Button> button = tgui::Button::create();
+		button->setText(std::get<2>(tuple));
+		button->connect("Pressed", [&, i]() {
+			onElementPressed.emit(this, i);
+		});
+		panel->add(button);
+		elements.push_back(std::make_tuple(std::get<0>(tuple), std::get<1>(tuple), button));
+		i++;
+	}
+
+	updateButtonsPositionsAndSizes();
+}
+
+tgui::Signal& ClickableTimeline::getSignal(std::string signalName) {
+	if (signalName == tgui::toLower(onElementPressed.getName())) {
+		return onElementPressed;
+	}
+	return tgui::Group::getSignal(signalName);
+}
+
+void ClickableTimeline::updateButtonsPositionsAndSizes() {
+	for (auto tuple : elements) {
+		std::shared_ptr<tgui::Button> button = std::get<2>(tuple);
+		button->setPosition(std::get<0>(tuple) * buttonScalar * buttonScalarScalar, 0);
+		button->setSize(std::get<1>(tuple) * buttonScalar * buttonScalarScalar, panel->getSize().y - 2);
+	}
+}
--- EditorUtilities.h
+++ EditorUtilities.h
@@ -30,6 +30,7 @@
 #include <thread>
 #include <mutex>
 #include <entt/entt.hpp>
+#include <tuple>
 
 /*
 Sends window to the foreground of the computer display.
@@ -541,4 +542,68 @@ private:
 
 	void onTabSelected(std::string tabName);
 	void updateMoreTabsList();
+};
+
+/*
+A timeline whose elements are clickable buttons.
+
+Signals:
+"ElementClicked" - emitted when an element is clicked; 
+	Optional parameter: the index of the element as given in setElements() (int)
+*/
+class ClickableTimeline : public tgui::Group {
+public:
+	ClickableTimeline();
+	static std::shared_ptr<ClickableTimeline> create() {
+		return std::make_shared<ClickableTimeline>();
+	}
+
+	/*
+	Sets the elements in this timeline. The duration of each element is assumed to be such that it ends right as 
+	the next element begins. The only exception is the last element, whose duration ends at maxTime.
+
+	itemStartTimes - the start time and text of every element;
+		this vector must be sorted in nondecreasing order by the start time and no element can overlap in time
+	maxDuration - the maximum time of the timeline
+	*/
+	void setElements(std::vector<std::pair<float, std::string>> elementStartTimes, float maxTime);
+
+	/*
+	Sets the elements in this timeline. The duration of each element must be given.
+	The timeline's max time is assumed to be the last element's start time added to its duration.
+
+	elementStartTimesAndDuration - a vector of tuples representing the start time, duration, and text of each element, respectively;
+		this vector must be sorted in nondecreasing order by the start time and no element can overlap in time
+	*/
+	void setElements(std::vector<std::tuple<float, float, std::string>> elementStartTimesAndDuration);
+
+	/*
+	Sets the elements in this timeline. The duration of each element must be given.
+
+	elementStartTimesAndDuration - a vector of tuples representing the start time, duration, and text of each element, respectively;
+		this vector must be sorted in nondecreasing order by the start time and no element can overlap in time
+	maxTime - the maximum time that can be displayed in the timeline
+	*/
+	void setElements(std::vector<std::tuple<float, float, std::string>> elementStartTimesAndDuration, float maxTime);
+
+	tgui::Signal& getSignal(std::string signalName) override;
+
+private:
+	tgui::SignalInt onElementPressed = { "ElementPressed" };
+
+	// A vector of tuples containing start time, duration, and corresponding Button of each element, in that order
+	// This vector is not guaranteed to be sorted in any way
+	std::vector<std::tuple<float, float, std::shared_ptr<tgui::Button>>> elements;
+
+	std::shared_ptr<tgui::ScrollablePanel> panel;
+	std::shared_ptr<tgui::RangeSlider> cameraController;
+
+	// How much to scale the x-coordinates and width of every button in the panel by
+	float buttonScalar = 1.0f;
+	// Another scalar that does the same thing; value comes from cameraController
+	float buttonScalarScalar = 1.0f;
+	// The maximum time value of the timeline
+	float maxTimeValue = 1.0f;
+
+	void updateButtonsPositionsAndSizes();
 };
