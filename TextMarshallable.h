a6b5c5762dfd14bcd909215a02e2960a1d4642df
Miv99 <treecam42@gmail.com>
2020-02-05 21:23:22 -0800

    Add utility string format function

--- TextMarshallable.h
+++ TextMarshallable.h
@@ -2,6 +2,20 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <memory>
+#include <stdexcept>
+
+/*
+Utility to imitate sprintf() but with std::string.
+*/
+template<typename ... Args>
+std::string format(const std::string& format, Args ... args) {
+	size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+	if (size <= 0) { throw std::runtime_error("Error during formatting."); }
+	std::unique_ptr<char[]> buf(new char[size]);
+	snprintf(buf.get(), size, format.c_str(), args ...);
+	return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+}
 
 #define tos(T) std::to_string(T)
 #define tm_delim std::string(1, DELIMITER)
