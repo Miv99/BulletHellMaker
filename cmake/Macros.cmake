0f0eed663341212db426e32c98d3d9123be2834d
Miv99 <treecam42@gmail.com>
2020-08-23 06:24:35 -0700

    Add cmake files

---
+++ cmake/Macros.cmake
@@ -0,0 +1,7 @@
+# Macro that helps defining an option
+macro(set_option var default type docstring)
+    if(NOT DEFINED ${var})
+        set(${var} ${default})
+    endif()
+    set(${var} ${${var}} CACHE ${type} ${docstring} FORCE)
+endmacro()
--- setup.bat
+++ setup.bat
@@ -1,8 +1,17 @@
-:: Copy all .dll files
-xcopy /y "setup-assets\*.dll" "BulletHellMaker"
+@setlocal enableextensions enabledelayedexpansion
 
-:: Create packages folder if it doesn't exist
-if not exist "BulletHellMaker\packages\" mkdir "BulletHellMaker\packages"
+:: Read setup config file
+set cfg=setup_config.txt
+for /F "usebackq eol=[ tokens=* delims=" %%a in ("%cfg%") do (
+    set "%%a"
+)
+
+:: Create folders if they don't exist
+if not exist "%CMAKE_BUILD_FOLDER%" mkdir "%CMAKE_BUILD_FOLDER%"
+if not exist "%CMAKE_BUILD_FOLDER%\packages\" mkdir "%CMAKE_BUILD_FOLDER%\packages"
+
+:: Copy all .dll files
+xcopy /y "setup-assets\*.dll" "%CMAKE_BUILD_FOLDER%"
 
 :: Unzip Python distribution to packages folder
-if not exist "BulletHellMaker\packages\Python27" powershell Expand-Archive "setup-assets\Python27.zip" -DestinationPath "BulletHellMaker\packages"
+if not exist "%CMAKE_BUILD_FOLDER%\packages\Python27" powershell Expand-Archive "setup-assets\Python27.zip" -DestinationPath "%CMAKE_BUILD_FOLDER%\packages"
