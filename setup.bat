35179cce830da93e610400ec30b9c29e020c8237
Miv99 <treecam42@gmail.com>
2020-08-23 14:41:55 -0700

    Change setup default folder

--- setup.bat
+++ setup.bat
@@ -7,11 +7,11 @@ for /F "usebackq eol=[ tokens=* delims=" %%a in ("%cfg%") do (
 )
 
 :: Create folders if they don't exist
-if not exist "%CMAKE_BUILD_FOLDER%" mkdir "%CMAKE_BUILD_FOLDER%"
-if not exist "%CMAKE_BUILD_FOLDER%\packages\" mkdir "%CMAKE_BUILD_FOLDER%\packages"
+if not exist "%EXE_TARGET_FOLDER%" mkdir "%EXE_TARGET_FOLDER%"
+if not exist "%EXE_TARGET_FOLDER%\packages\" mkdir "%EXE_TARGET_FOLDER%\packages"
 
 :: Copy all .dll files
-xcopy /y "setup-assets\*.dll" "%CMAKE_BUILD_FOLDER%"
+xcopy /y "setup-assets\*.dll" "%EXE_TARGET_FOLDER%"
 
 :: Unzip Python distribution to packages folder
-if not exist "%CMAKE_BUILD_FOLDER%\packages\Python27" powershell Expand-Archive "setup-assets\Python27.zip" -DestinationPath "%CMAKE_BUILD_FOLDER%\packages"
+if not exist "%EXE_TARGET_FOLDER%\packages\Python27" powershell Expand-Archive "setup-assets\Python27.zip" -DestinationPath "%EXE_TARGET_FOLDER%\packages"
