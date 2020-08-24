b5b00a00463d75e93b79cb8f43aef2a181ecf8c0
Miv99 <treecam42@gmail.com>
2020-08-24 04:12:03 -0700

    Change cmake folders and add tests to cmake

--- setup.bat
+++ setup.bat
@@ -10,8 +10,16 @@ for /F "usebackq eol=[ tokens=* delims=" %%a in ("%cfg%") do (
 if not exist "%EXE_TARGET_FOLDER%" mkdir "%EXE_TARGET_FOLDER%"
 if not exist "%EXE_TARGET_FOLDER%\packages\" mkdir "%EXE_TARGET_FOLDER%\packages"
 
-:: Copy all .dll files
-xcopy /y "setup-assets\*.dll" "%EXE_TARGET_FOLDER%"
+:: Unzip common-dlls, debug-dlls, and release-dlls into game folder
+powershell Expand-Archive -Force "setup-assets\common-dlls.zip" -DestinationPath "%EXE_TARGET_FOLDER%"
+powershell Expand-Archive -Force "setup-assets\debug-dlls.zip" -DestinationPath "%EXE_TARGET_FOLDER%"
+powershell Expand-Archive -Force "setup-assets\release-dlls.zip" -DestinationPath "%EXE_TARGET_FOLDER%"
+
+:: Unzip BulletHellMaker assets to game folder
+if not exist "%EXE_TARGET_FOLDER%\Shaders" powershell Expand-Archive -Force "setup-assets\Game-Assets.zip" -DestinationPath "%EXE_TARGET_FOLDER%"
+
+:: Unzip sample Level Packs to game folder
+if not exist "%EXE_TARGET_FOLDER%\Level Packs" powershell Expand-Archive -Force "setup-assets\Level-Packs.zip" -DestinationPath "%EXE_TARGET_FOLDER%"
 
 :: Unzip Python distribution to packages folder
-if not exist "%EXE_TARGET_FOLDER%\packages\Python27" powershell Expand-Archive "setup-assets\Python27.zip" -DestinationPath "%EXE_TARGET_FOLDER%\packages"
+if not exist "%EXE_TARGET_FOLDER%\packages\Python27" powershell Expand-Archive -Force "setup-assets\Python27.zip" -DestinationPath "%EXE_TARGET_FOLDER%\packages"
